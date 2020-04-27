d69a8b2fe14c815129e9cdaabfdb845511de8399
Yoppy61 <cableguyjr61@gmail.com>
2020-04-27 16:42:19 +0900

    Table表示

--- App.tsx
+++ App.tsx
@@ -1,34 +1,84 @@
-import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {db} from './config';
+import React from 'react';
+import { SafeAreaView, View, FlatList, StyleSheet, Text } from 'react-native';
+import Constants from 'expo-constants';
 
-export default class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.red}>Hello world!</Text>
-        <table>
-          <tr><td>ラーメン</td></tr>
-          <tr><td>ラーメン</td></tr>
-          <tr><td>ラーメン</td></tr>
-        </table>
-      </View>
-    );
-  }
+const DATA = [
+  {
+    id: '1',
+    title: 'りゅうおう',
+  },
+  {
+    id: '2',
+    title: 'シドー',
+  },
+  {
+    id: '3',
+    title: 'ゾーマ',
+  },
+  {
+    id: '4',
+    title: 'デスピサロ',
+  },
+  {
+    id: '5',
+    title: 'ミルドラース',
+  },
+  {
+    id: '6',
+    title: 'デスタムーア',
+  },
+  {
+    id: '7',
+    title: 'オルゴ・デミーラ',
+  },
+  {
+    id: '8',
+    title: 'ラプソーン',
+  },
+  {
+    id: '9',
+    title: 'エルギオス',
+  },
+  {
+    id: '10',
+    title: 'マデサゴーラ',
+  },
+];
+
+{/* typescriptのルールで、any型(なんでも有りの型)はNGの為、コンパイルは通るがコード上ではエラー*/}
+function Item({ title }) {
+  return (
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={({ item }) => <Item title={item.title} />}
+        keyExtractor={item => item.id}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'column',
-    margin: 'auto'
+    flex: 1,
+    marginTop: Constants.statusBarHeight,
   },
-
-  red: {
-    backgroundColor: "#ffffff",
-    color: "#ff0000",
+  item: {
+    backgroundColor: '#4169e1',
+    padding: 20,
+    marginHorizontal: 8,
+    borderWidth: 1,
+    borderColor: '#000000',
   },
-
-  table: {
-    color: "#ff0000"
+  title: {
+    fontSize: 32,
   },
 });
